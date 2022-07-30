825633aeb950a5023bb2d3bba9a694fc6fe51383
goku-g <gokarnabaskota500@gmail.com>
2022-07-31 03:03:14 +0545

    format the output

--- lab-1-intro_python/5-student_marks_display.py
+++ lab-1-intro_python/5-student_marks_display.py
@@ -1,13 +1,13 @@
 st_info = {}
-num = 1
+num = int(input("How many student over there?\n"))
 
 for i in range(num):
     name = input("Enter student Name: ")
     st_info[name] = input("Enter student Mark: ")
 
-print("SN \t| Name \t\t\t| Mark")
-print("---------------------------------------")
+print("SN \t| Name \t\t\t\t| Mark")
+print("----------------------------------------------")
 i = 1
 for key in st_info.keys():
-    print("{} \t| {} \t\t\t| {}".format(i, key.upper(), st_info[key]))
+    print("{} \t| {} \t\t| {}".format(i, key.upper(), st_info[key]))
     i = i + 1
